c3c33b5e83c734442fb42c6ec52ecca3277f759a
Chuchundrium <55204077+Chuchundrium@users.noreply.github.com>
2023-05-24 19:45:05 +0300

    2 files modified

--- src/patterns.service.ts
+++ src/patterns.service.ts
@@ -306,17 +306,16 @@ export function setCustomPattern(
   ctx: CanvasRenderingContext2D,
   lineWidth = 2,
   backgroundColor = '#F2B6A050',
-  lineColor = '#850000'
+  lineColor = 'rgb(30, 70, 200)'
 ) {
   const patternCanvas = document.createElement('canvas');
   const patternCtx = patternCanvas.getContext('2d') as CanvasRenderingContext2D;
 
   const img = new Image();
-  // img.src = 'assets/tile.svg';
 
   img.src =
     'data:image/svg+xml; charset=utf8,' +
-    CUSTOM_PATTERNS_MAPPING['GRAVEL']('rgba(30, 70, 200)', lineWidth);
+    CUSTOM_PATTERNS_MAPPING['GRAVEL'](lineColor, lineWidth);
 
   img.onload = () => {
     patternCanvas.width = img.width;
@@ -328,4 +327,27 @@ export function setCustomPattern(
     ctx.fillStyle = ctx.createPattern(patternCanvas, 'repeat') as CanvasPattern;
     ctx.fillRect(0, 0, 300, 300);
   };
+}
+
+function renderLinkIcon() {
+  const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const iconPath = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  );
+
+  iconSvg.setAttribute('fill', 'none');
+  iconSvg.setAttribute('viewBox', '0 0 24 24');
+  iconSvg.setAttribute('stroke', 'black');
+  iconSvg.classList.add('post-icon');
+
+  iconPath.setAttribute(
+    'd',
+    'M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1'
+  );
+  iconPath.setAttribute('stroke-linecap', 'round');
+  iconPath.setAttribute('stroke-linejoin', 'round');
+  iconPath.setAttribute('stroke-width', '2');
+
+  iconSvg.appendChild(iconPath);
 }
